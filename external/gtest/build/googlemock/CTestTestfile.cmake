fa4405f7a5aea966781b380479efa80362417a51
/Chenxuan <liu00464@umn.edu>
2020-02-28 16:40:59 -0600

    lab9 push1

---
+++ external/gtest/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/liu00464/3081_s20/repo-liu00464/external/gtest/src/googlemock
+# Build directory: /home/liu00464/3081_s20/repo-liu00464/external/gtest/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
